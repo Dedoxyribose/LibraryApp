4981c155dbf6530fae64cc89a21850870702131f
Dedoxyribose <dedoxyribose@gmail.com>
2022-10-02 15:06:15 +0300

    initial commit

---
+++ app/src/main/java/com/dedoxyribose/library/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.dedoxyribose.library.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple = Color(0xFF2D142C)
+val PurpleLighter = Color(0xFF510A32)
+val PurpleLightest = Color(0xFF801336)
+val Apricot = Color(0xFFEE4540)
